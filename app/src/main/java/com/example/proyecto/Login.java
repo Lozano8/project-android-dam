87392d7a55652fc730212d69d1dee61a0935fd57
Cristi Baque <86905714+Sele97@users.noreply.github.com>
2022-07-17 01:45:50 -0500

    Nuevas ventanas
    completar ventanas de modulos emprendedor, emprendimiento

--- app/src/main/java/com/example/proyecto/Login.java
+++ app/src/main/java/com/example/proyecto/Login.java
@@ -3,12 +3,22 @@ package com.example.proyecto;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class Login extends AppCompatActivity {
+
+    EditText usuario, contraseña;
+    Button iniciarsesion, registrarse;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        usuario = findViewById(R.id.Usuario);
+        contraseña =findViewById(R.id.password);
+
+
     }
 }
