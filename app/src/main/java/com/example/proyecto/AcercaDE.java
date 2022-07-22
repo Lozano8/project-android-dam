516b64e43633bca5224c314d05e64336757998a1
AluBry12 <bryan12bmw@gmail.com>
2022-07-22 03:08:39 -0500

    final

---
+++ app/src/main/java/com/example/proyecto/AcercaDE.java
@@ -0,0 +1,14 @@
+package com.example.proyecto;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AcercaDE extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_acerca_de2);
+    }
+}
